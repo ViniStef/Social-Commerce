03d7e6d4cd6b5e8fac1efa58ac997df6b1d23e1c
Vinicius <145880205+ViniStef@users.noreply.github.com>
2024-10-26 14:52:46 -0300

    First project setup with Spring Boot

---
+++ src/main/java/com/socialcommerce/socialcommerce/SocialCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.socialcommerce.socialcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialCommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialCommerceApplication.class, args);
+    }
+
+}
